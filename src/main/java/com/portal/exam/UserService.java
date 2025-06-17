5040b1700ee8384b85bc731183dc33a4b3ff9b30
Anirudhkrishna0612 <anirudhkrishna0612@gmail.com>
2025-06-17 13:05:24 +0530

    Solved all the error

--- src/main/java/com/portal/exam/UserService.java
+++ src/main/java/com/portal/exam/UserService.java
@@ -6,5 +6,9 @@ public interface UserService {
 	
 	//Creating user
 	public User createUser(User user,Set<UserRole> userRoles) throws Exception;
+	//Get user by username 
+	public User getUser(String username);
+	//delete user by id
+	public void deleteUser(Long userId);
 
 }
